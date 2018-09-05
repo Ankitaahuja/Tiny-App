ccc25a081c7c3941d9a0d0d3b4a431fed7419309
Ankita Ahuja <ankitaahuja111@gmail.com>
2018-09-05 18:22:47 -0400

    Implemented Logout Client & Server logic

--- express_server.js
+++ express_server.js
@@ -32,12 +32,15 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {  username: req.cookies["username"], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -48,7 +51,7 @@ app.post("/urls", (req, res) => {
 });
 // console.log(urlDatabase);
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id, urls: urlDatabase };
+  let templateVars = { username: req.cookies["username"], shortURL: req.params.id, urls: urlDatabase };
   res.render("urls_show", templateVars);
 });
 
@@ -70,6 +73,12 @@ app.post("/urls/:id", (req, res) => {
 app.post("/login", (req, res) => {
   res.cookie('username', req.body.username);
   
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username")
+  //clear.cookie('username', req.body.username);
   res.redirect("/urls");
 });
 
